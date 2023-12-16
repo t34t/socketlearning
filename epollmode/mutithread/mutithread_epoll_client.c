3e1582d7dfe0a3907e92438158bfdc75e81410db
river <928885815@qq.com>
2023-12-16 17:03:57 +0800

    change some comments

--- epollmode/mutithread/mutithread_epoll_client.c
+++ epollmode/mutithread/mutithread_epoll_client.c
@@ -44,7 +44,7 @@ int main(int argc,const char * argv[])
         usleep(100000); 
     }
 
-    printf("------over-------");
+    printf("------program over-------");
     close(fd);
     
     return 0;
